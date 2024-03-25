f6281b8c1c7a18045afd8475e90999635b934c87
thereakhesham <heshammoha231992@gmail.com>
2024-03-25 20:51:54 +0200

    commit on master

--- src/app.jsx
+++ src/app.jsx
@@ -26,7 +26,7 @@ return(
 						<Route path={`/auth`} component={AuthLayout} />
 						<Route path={`/admin`} component={AdminLayout} />
 						<Route path={`/rtl`} component={RtlLayout} />
-						<Route path={`/`} component={auth} />
+						<Route path={`/`} component={AuthLayout} />
 		
 				</BrowserRouter>
 			</ThemeEditorProvider>
